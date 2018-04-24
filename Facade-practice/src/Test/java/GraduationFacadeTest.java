dd4ace5a87a28631c47194f4a908c123cfa335d3
JulianLz96 <jj_lz96@hotmail.com>
2018-04-23 19:59:16 -0500

    test unit

--- Facade-practice/src/Test/java/GraduationFacadeTest.java
+++ Facade-practice/src/Test/java/GraduationFacadeTest.java
@@ -1,25 +1,41 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+
 public class GraduationFacadeTest {
 
-    GraduationFacade graduationFacade;
-    Banquet banquet;
-    Decoration decoration;
-    Drinks drinks;
-    Flowers flowers;
-    LightControl lightControl;
-    Waiter waiter;
-    MusicBand musicBand;
-    DjAdapter djAdapter;
+    private GraduationFacade graduationFacade;
+    private Banquet mokedBanquet;
+    private Decoration mockedDecoration;
+    private Drinks mockedDrinks;
+    private Flowers mockedFlowers;
+    private LightControl mockedLightControl;
+    private Waiter mockedWaiter;
+    private MusicBand mockedMusicBand;
+    private DjAdapter djAdapter;
 
     @Before
     public void setUp(){
-
+    mokedBanquet = mock(Banquet.class);
+    mockedDecoration = mock(Decoration.class);
+    mockedDrinks = mock(Drinks.class);
+    mockedFlowers = mock(Flowers.class);
+    mockedLightControl = mock(LightControl.class);
+    mockedWaiter = mock(Waiter.class);
+    mockedMusicBand = mock(MusicBand.class);
+    djAdapter = new DjAdapter();
+    djAdapter.setDj(mock(DJ.class));
     }
 
     @Test
-    public void organizeSetUpTest(){
-        //graduationFacade = new GraduationFacade();
+    public void organizeSetUpTestwithMusicBand(){
+        graduationFacade.setMusicBand(mockedMusicBand);
+        graduationFacade.organiceGraduation();
+    }
+
+    public void organizeGraduationwithDJ(){
+        graduationFacade.setMusicBand(djAdapter);
+
     }
 }
